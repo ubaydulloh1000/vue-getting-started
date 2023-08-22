8ab3aaf9eac212588fd83a914d0e42199e996df3
Ubaydulloh Kuchkarov <ubaydulloh1000@gmail.com>
2023-08-22 15:35:31 +0500

    props, slots and some changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ hello_msg }}</h1>
+  <!-- <div>
+    <input ref="hello"/>
+  </div> -->
+  <button class="toggleButton" @click="toggleModal">toggle modal</button>
+  <button class="toggleButton button-2" @click="toogleModalTwo">toggle modal two</button>
+  <div v-if="showModal">
+    <MyModal :type="type" @close-modal="toggleModal">
+      <h1>Hello World!</h1>
+      <template v-slot:links>
+        <a class="link-button" href="#">sign up</a>
+        <a class="link-button" href="#">more info</a>
+      </template>
+    </MyModal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <MyModal :header="model_header" @close-modal="toogleModalTwo">
+      <h1>
+        This is second modal.
+      </h1>
+      <template v-slot:links>
+        <a class="link-button">
+            register
+        </a>
+      </template>
+    </MyModal>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyModal from "./components/MyModal.vue"
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {MyModal},
+  data() {
+    return {
+      hello_msg: "This is hello message.",
+      model_header: "This is props model header.",
+      type: "success",
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toogleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
   }
 }
 </script>
@@ -22,5 +65,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
+}
+.toggleButton{
+  margin: 0 5px;
+  padding: 10px 15px;
+  font-size: 16px;
+}
+.link-button{
+  margin: 0 5px;
+  padding: 8px 15px;
+  background: rgba(0, 0, 0, 0.3);
+  color: white;
+}
+.link-button:hover{
+  background: rgba(0, 0, 0, 0.4);
 }
 </style>
---
+++ src/components/MyModal.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="backdrop" @click.self="hideMyModal">
+        <div class="modal" :class="{success: type === 'success'}" ref="myModal">
+        <div class="cancel-container">
+            <button class="cancel" v-on:click="hideMyModal">
+                <span>x</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <slot>default content</slot>
+            <slot name="links"></slot>
+        </div>
+    </div>
+    </div>
+    
+</template>
+
+
+<script>
+export default {
+    name: "MyModal",
+    props: {
+        header: String,
+        msg: String,
+        type: String
+    },
+    methods: {
+        hideMyModal(){
+            this.$emit("close-modal");
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+.backdrop{
+    top: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.2);
+}
+.modal {
+    background-color: #fff;
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+    border-radius: 8px;
+}
+.modal.error {
+    background: rgba(151, 44, 44, 1);
+    color: white;
+}
+.modal.success{
+    background: rgb(9, 175, 9);
+    color: white;
+}
+.modal .cancel-container{
+    display: flex;
+    flex-direction: row-reverse;
+}
+.modal .cancel-container button {
+    color: red;
+    font-size: 22px;
+    width: 40px;
+    height: 40px;
+}
+
+</style>
